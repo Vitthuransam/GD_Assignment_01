a9ff4da8dfd4b077c14086b1854cb2dbd4bdcc90
Vithuran Sam <vitthuransam@gmail.com>
2022-04-25 20:34:15 +0530

    2nd Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Room camera
+    [SerializeField] private float speed;
+    private float currentPosX;
+ 
+
+    //Follow player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }
+
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
+    }
+}
